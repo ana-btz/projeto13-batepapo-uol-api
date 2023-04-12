77725a14ddd54b3143bfd55193b03d4f406ec4c4
ana_btz <silva.anabtz@gmail.com>
2023-04-12 03:40:39 -0300

    feat: connect with database

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
 
 const app = express();
+const PORT = 5000;
 
 app.use(cors);
+app.use(express.json());
 
-const PORT = 5000;
+// Conectar com DB
+let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch(err => console.log(err.message));
+
 app.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`));
